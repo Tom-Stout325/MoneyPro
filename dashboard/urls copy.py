a8b041328dc2ab88006054462ad5f183b0f0da28
Tom <tom.stout325@gmail.com>
2026-03-18 22:02:55 -0400

    updated urls

--- dashboard/urls copy.py
+++ dashboard/urls copy.py
@@ -1,10 +1,12 @@
 from django.urls import path
+
 from .views import dashboard_chart_data, dashboard_home, reseed_defaults, rebuild_defaults
 
 app_name = "dashboard"
 
 urlpatterns = [
     path("", dashboard_home, name="home"),
+    path("", dashboard_home, name="dashboard_home"),
     path("chart-data/", dashboard_chart_data, name="chart_data"),
     path("seed-defaults/", reseed_defaults, name="seed_defaults"),
     path("rebuild-defaults/", rebuild_defaults, name="rebuild_defaults"),
