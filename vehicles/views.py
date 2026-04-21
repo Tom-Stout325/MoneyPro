abb1485a2f74fbbc07575f1afb26e35c5b43edba
Tom <tom.stout325@gmail.com>
2026-04-20 22:44:47 -0400

    fixed year bug

--- vehicles/views.py
+++ vehicles/views.py
@@ -386,9 +386,9 @@ class VehicleMilesListView(LoginRequiredMixin, ListView):
         ctx = super().get_context_data(**kwargs)
         year = _parse_year(self.request.GET.get("year"))
         vehicle_filter = self.request.GET.get("vehicle") or ""
-        page_qs = ctx["miles_entries"].object_list if hasattr(ctx["miles_entries"], "object_list") else ctx["miles_entries"]
-        total_logged = page_qs.aggregate(total=Sum("total"))["total"] or ZERO_TENTH
-        total_business = page_qs.filter(mileage_type=VehicleMiles.MileageType.BUSINESS).aggregate(total=Sum("total"))["total"] or ZERO_TENTH
+        totals_qs = self.get_queryset()
+        total_logged = totals_qs.aggregate(total=Sum("total"))["total"] or ZERO_TENTH
+        total_business = totals_qs.filter(mileage_type=VehicleMiles.MileageType.BUSINESS).aggregate(total=Sum("total"))["total"] or ZERO_TENTH
         ctx.update({
             "year": year,
             "year_choices": _year_choices(),
